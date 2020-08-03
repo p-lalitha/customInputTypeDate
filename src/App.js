096b08a161f06c808652a75f4a8747eaf514540c
Lalitha Perumalla <31858856+p-lalitha@users.noreply.github.com>
2020-08-03 20:32:29 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Customcalender from "./components/customcalender";
+
+function App() {
+  return <Customcalender />;
+}
+
+export default App;
